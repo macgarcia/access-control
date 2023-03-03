a48ea95f6d232c4378c5444fe3e2be5292ffa2ae
macgarcia <marquim.iron@gmail.com>
2023-03-03 18:31:05 -0300

    Configuration for windows and linux

--- install.bat
+++ install.bat
@@ -1,9 +1,8 @@
 mkdir obj
 mkdir bin
 
-gcc.exe -Wall -g  -c main.c -o obj\main.o
-gcc.exe -Wall -g  -c note_behavior.c -o obj\note_behavior.o
-gcc.exe -Wall -g  -c note_repo_txt.c -o obj\note_repo_txt.o
-gcc.exe -Wall -g  -c note_repository.c -o obj\note_repository.o
-gcc.exe -Wall -g  -c configurations.c -o obj\configurations.o
-gcc.exe  -o bin\manager-access.exe obj\main.o obj\note_behavior.o obj\note_repo_txt.o obj\note_repository.o obj\configurations.o
+gcc -Wall -g -c src\main.c -o obj\main.o
+gcc -Wall -g -c src\configuration.c -o obj\configuration.o
+gcc -Wall -g -c src\menu.c -o obj\menu.o
+
+gcc -o bin\access-control.exe obj\main.o obj\configuration.o obj\menu.o
--- src/configuration.c
+++ src/configuration.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+
 #include "../include/configuration.h"
 
 void clear_screen() {

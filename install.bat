c102600f4659adaf5d03a6f8b9629cd1c70c30ab
macgarcia <marquim.iron@gmail.com>
2023-03-06 12:45:11 -0300

    Configuration to compile on windows and linux.

--- install.bat
+++ install.bat
@@ -4,5 +4,9 @@ mkdir bin
 gcc -Wall -g -c src\main.c -o obj\main.o
 gcc -Wall -g -c src\configuration.c -o obj\configuration.o
 gcc -Wall -g -c src\menu.c -o obj\menu.o
+gcc -Wall -g -c src\note_behavior.c -o obj\note_behavior.o
+gcc -Wall -g -c src\note_list.c -o obj\note_list.o
+gcc -Wall -g -c src\note_repository.c -o obj\note_repository.o
+gcc -Wall -g -c src\text_file.c -o obj\text_file.o
 
-gcc -o bin\access-control.exe obj\main.o obj\configuration.o obj\menu.o
+gcc -o bin\access-control obj\main.o obj\configuration.o obj\menu.o obj\note_behavior.o obj\note_list.o obj\note_repository.o obj\text_file.o
--- install.sh
+++ install.sh
@@ -3,15 +3,12 @@
 mkdir obj
 mkdir bin
 
-#gcc -Wall -g -c main.c -o obj/main.o
-#gcc -Wall -g -c note_behavior.c -o obj/note_behavior.o
-#gcc -Wall -g -c note_repo_txt.c -o obj/note_repo_txt.o
-#gcc -Wall -g -c note_repository.c -o obj/note_repository.o
-#gcc -Wall -g -c configurations.c -o obj/configurations.o
-#gcc -o bin/manager-access obj/main.o obj/note_behavior.o obj/note_repo_txt.o obj/note_repository.o obj/configurations.o
-
 gcc -Wall -g -c src/main.c -o obj/main.o
 gcc -Wall -g -c src/configuration.c -o obj/configuration.o
 gcc -Wall -g -c src/menu.c -o obj/menu.o
+gcc -Wall -g -c src/note_behavior.c -o obj/note_behavior.o
+gcc -Wall -g -c src/note_list.c -o obj/note_list.o
+gcc -Wall -g -c src/note_repository.c -o obj/note_repository.o
+gcc -Wall -g -c src/text_file.c -o obj/text_file.o
 
-gcc -o bin/access-control obj/main.o obj/configuration.o obj/menu.o
+gcc -o bin/access-control obj/main.o obj/configuration.o obj/menu.o obj/note_behavior.o obj/note_list.o obj/note_repository.o obj/text_file.o

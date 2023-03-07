b5cef2a8ac0870a8a591a6c8a77100f4ce479a43
macgarcia <marquim.iron@gmail.com>
2023-03-07 02:31:45 -0300

    Corretions to execute all tests on linux

--- test/test.c
+++ test/test.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 
 int main(void) {
-    #ifndef __linux__
+    #ifdef __linux__
         system("./text_file_test");
     #else
         system("text_file_test.exe");
-    #endif // __linux__
+    #endif
     return 0;
 }

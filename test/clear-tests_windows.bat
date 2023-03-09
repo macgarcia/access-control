d32f0d67001963ba33caf6d8154c2622597ae8af
macgarcia <marquim.iron@gmail.com>
2023-03-09 00:26:30 -0300

    Configurations to test on windows

--- test/clear-tests_windows.bat
+++ test/clear-tests_windows.bat
@@ -2,3 +2,4 @@ rmdir /s /q test-obj
 del .notes.txt
 del tests.exe
 del text_file_test.exe
+del note_list_test.exe
--- test/tests_windows.bat
+++ test/tests_windows.bat
@@ -6,7 +6,12 @@ gcc -Wall -g -c ..\src\text_file.c -o test-obj\text_file.o
 gcc -Wall -g -c text_file_test.c -o test-obj\text_file_test.o
 gcc -Wall -g -c unit\unity.c -o test-obj\unity.o
 
-gcc.exe -o text_file_test.exe test-obj\text_file.o test-obj\text_file_test.o test-obj\unity.o   
+gcc.exe -o text_file_test.exe test-obj\text_file.o test-obj\text_file_test.o test-obj\unity.o
+
+gcc -Wall -g -c ..\src\note_list.c -o test-obj\note_list.o
+gcc -Wall -g -c note_list_test.c -o test-obj\note_list_test.o
+
+gcc -o note_list_test.exe test-obj\note_list.o test-obj\note_list_test.o test-obj\unity.o
 
 gcc -Wall -g -c test.c -o test-obj\test.o
 gcc.exe -o tests test-obj\test.o
